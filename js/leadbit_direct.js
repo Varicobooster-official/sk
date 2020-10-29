fe60d09dea2245ef6666e37a7014b7d162af20ec
Varicobooster-official <73607312+Varicobooster-official@users.noreply.github.com>
2020-10-29 11:51:42 +0200

    1

---
+++ js/leadbit_direct.js
@@ -0,0 +1,9 @@
+(function($) {
+    'use strict';
+
+    $(function () {
+        var script = document.createElement('script');
+        script.src = '/cdn/js/lea'+'dbit.js?v=1';
+        document.body.appendChild(script);
+    });
+})( jQuery );
